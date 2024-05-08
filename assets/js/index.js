e2ef51b62f210b9e511ae4b76f8eacc1488b617b
Huzyefah <huzyefahsaqib13@gmail.com>
2024-05-08 20:48:05 +0500

    efwef

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,23 @@
 const survey = new Survey.Model(json);
 survey.applyTheme(themeJson);
-
-function sendDataToServer(survey) {
-    // Extract data from the survey
-    const data = {
-        "City": survey.data.City,
-        "State": survey.data.State,
-        "Country": survey.data.Country,
-        "Name": survey.data.Name,
-        "Email": survey.data.Email,
-        "Phone Number": survey.data["Phone Number"],
-        "Address": survey.data.Address,
-        "Apt": survey.data.Apt,
-        "Service": survey.data.Service,
-        "Time": survey.data.Time
-        // Add other form fields as needed
-    };
-
-    // Send the data to your server
-    $.ajax({
-        url: 'https://script.google.com/macros/s/AKfycbx0ec7cxIIASQeuXLbnh1Ocb2PvxjBZ-EEZMbcI1lnwYJ22l5eP2tNGtTrYP4nG5-hHxw/exec',
-        type: 'post',
-        contentType: 'application/json', // Specify content type as JSON
-        data: JSON.stringify(data), // Stringify the data object
-        success: function(response) {
-            console.log('Data sent successfully:', response);
-            // Optionally, handle the success response
-        },
-        error: function(xhr, status, error) {
-            console.error('Error sending data:', error);
-            // Optionally, handle the error
-        }
-    });
-}
-
-
 survey.onComplete.add((sender, options) => {
-    sendDataToServer(sender); // Call sendDataToServer when the survey is completed
+    const data = sender.data;
+    console.log(JSON.stringify(data, null, 3));
+
+    // Make a POST request to the Google Apps Script
+    fetch("https://script.google.com/macros/s/AKfycbxSb9q10iqZ5Q1oYwCgJE-SrsKJ84ETLqvtA7-_-3hZ1z47Nx4m4-pGHu-OHYtASHX00g/exec", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.error('Error:', error));
 });
 
 $("#surveyElement").Survey({ model: survey });
+
+
+
