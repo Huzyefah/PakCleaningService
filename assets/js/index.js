67d80521a867c07a939aa9cda3192a25398334f2
Huzyefah <huzyefahsaqib13@gmail.com>
2024-05-09 17:23:28 +0500

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,22 +2,28 @@ const survey = new Survey.Model(json);
 survey.applyTheme(themeJson);
 survey.onComplete.add((sender, options) => {
     const data = sender.data;
-    console.log(JSON.stringify(data, null, 3));
-
-    // Make a POST request to the Google Apps Script
-    fetch("https://script.google.com/macros/s/AKfycbxSb9q10iqZ5Q1oYwCgJE-SrsKJ84ETLqvtA7-_-3hZ1z47Nx4m4-pGHu-OHYtASHX00g/exec", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.error('Error:', error));
+    console.log('Survey data:', data); // Debugging statement
+    submitSurvey(data);
 });
 
+const submitSurvey = async (data) => {
+    try {
+        console.log('Submitting survey data:', data); // Debugging statement
+        const response = await fetch('/submit-survey', { // <-- Ensure the URL is correct here
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            console.log('Survey submitted successfully!');
+        } else {
+            console.error('Failed to submit survey:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error submitting survey:', error);
+    }
+};
+
 $("#surveyElement").Survey({ model: survey });
-
-
-
